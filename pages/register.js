83dbb2b4c767fc7d04ea93d0e54031f9cf97dab6
wachid <wachidnurafif29@gmail.com>
2022-08-01 13:43:24 +0700

    instalation next

---
+++ pages/register.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Register() {
+  return (
+    <>
+      <div className="mb-3">
+        <h2>Register</h2>
+      </div>
+      <div>
+        <h2>test</h2>
+      </div>
+    </>
+  );
+}
+
+export default Register;
