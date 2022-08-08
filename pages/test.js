5f129baeb04393bf3edf3bc140592bf37df36450
wachid <wachidnurafif29@gmail.com>
2022-08-08 23:50:29 +0700

    [Add] Integrasi API

---
+++ pages/test.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function SyncSlider() {
+  const [nav1, setNav1] = useState();
+  const [nav2, setNav2] = useState();
+
+  return <div></div>;
+}
